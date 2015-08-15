039fce4410ec07238e8fbe54aba25b7b3dd73273
anton164 <anton_abilov@hotmail.com>
2015-08-15 14:45:53 +1000

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('chefupApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
