dddf3059401a49d604925f6a82581186aad2e47c
Adam Malcontenti-Wilson <adman.com@gmail.com>
2015-08-15 15:39:32 +1000

    Rip out local auth, fix facebook auth

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -2,8 +2,7 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var crypto = require('crypto');
-var authTypes = ['github', 'twitter', 'facebook', 'google'];
+var authTypes = ['facebook'];
 
 var UserSchema = new Schema({
   name: String,
@@ -12,26 +11,13 @@ var UserSchema = new Schema({
     type: String,
     default: 'user'
   },
-  hashedPassword: String,
   provider: String,
-  salt: String,
   facebook: {},
-  github: {}
 });
 
 /**
  * Virtuals
  */
-UserSchema
-  .virtual('password')
-  .set(function(password) {
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashedPassword = this.encryptPassword(password);
-  })
-  .get(function() {
-    return this._password;
-  });
 
 // Public profile information
 UserSchema
@@ -57,22 +43,6 @@ UserSchema
  * Validations
  */
 
-// Validate empty email
-UserSchema
-  .path('email')
-  .validate(function(email) {
-    if (authTypes.indexOf(this.provider) !== -1) return true;
-    return email.length;
-  }, 'Email cannot be blank');
-
-// Validate empty password
-UserSchema
-  .path('hashedPassword')
-  .validate(function(hashedPassword) {
-    if (authTypes.indexOf(this.provider) !== -1) return true;
-    return hashedPassword.length;
-  }, 'Password cannot be blank');
-
 // Validate email is not taken
 UserSchema
   .path('email')
@@ -87,61 +57,5 @@ UserSchema
       respond(true);
     });
 }, 'The specified email address is already in use.');
-
-var validatePresenceOf = function(value) {
-  return value && value.length;
-};
-
-/**
- * Pre-save hook
- */
-UserSchema
-  .pre('save', function(next) {
-    if (!this.isNew) return next();
-
-    if (!validatePresenceOf(this.hashedPassword) && authTypes.indexOf(this.provider) === -1)
-      next(new Error('Invalid password'));
-    else
-      next();
-  });
-
-/**
- * Methods
- */
-UserSchema.methods = {
-  /**
-   * Authenticate - check if the passwords are the same
-   *
-   * @param {String} plainText
-   * @return {Boolean}
-   * @api public
-   */
-  authenticate: function(plainText) {
-    return this.encryptPassword(plainText) === this.hashedPassword;
-  },
-
-  /**
-   * Make salt
-   *
-   * @return {String}
-   * @api public
-   */
-  makeSalt: function() {
-    return crypto.randomBytes(16).toString('base64');
-  },
-
-  /**
-   * Encrypt password
-   *
-   * @param {String} password
-   * @return {String}
-   * @api public
-   */
-  encryptPassword: function(password) {
-    if (!password || !this.salt) return '';
-    var salt = new Buffer(this.salt, 'base64');
-    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
-  }
-};
 
 module.exports = mongoose.model('User', UserSchema);
--- server/auth/facebook/passport.js
+++ server/auth/facebook/passport.js
@@ -5,9 +5,13 @@ exports.setup = function (User, config) {
   passport.use(new FacebookStrategy({
       clientID: config.facebook.clientID,
       clientSecret: config.facebook.clientSecret,
-      callbackURL: config.facebook.callbackURL
+      callbackURL: config.facebook.callbackURL,
+      profileFields: ['id', 'displayName', 'email'],
+      enableProof: true
     },
     function(accessToken, refreshToken, profile, done) {
+
+
       User.findOne({
         'facebook.id': profile.id
       },
@@ -16,11 +20,11 @@ exports.setup = function (User, config) {
           return done(err);
         }
         if (!user) {
+          console.log(profile);
           user = new User({
             name: profile.displayName,
             email: profile.emails[0].value,
             role: 'user',
-            username: profile.username,
             provider: 'facebook',
             facebook: profile._json
           });
