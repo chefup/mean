44f3acd866518ac9f93f358c1b85c3b197bb6182
anton164 <anton_abilov@hotmail.com>
2015-08-15 17:46:31 +1000

    Added location lookup to navbar

---
+++ client/components/navbar/lookup.directive.js
@@ -0,0 +1,21 @@
+angular.module("chefupApp").directive("locationLookup", ['$http',
+  function($http) {
+    return {
+      restrict: 'E',
+      templateUrl: 'components/navbar/locationlookup.html',
+      scope: {},
+      link: function($scope, element, attrs) {
+        var $input = $("#locationLookup");
+        $http.get('http://ip-api.com/json').then(function(response) {
+          $input.val(response.data.city + ', ' + response.data.regionName + ', ' + response.data.country)
+        });
+        var autocomplete = new google.maps.places.Autocomplete($input.get(0), {});
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+          var place = autocomplete.getPlace();
+          $scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
+          $scope.$apply();
+        });
+      }
+    };
+  }
+]);
